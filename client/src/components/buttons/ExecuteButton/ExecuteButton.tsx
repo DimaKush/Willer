876464f10f0928c8df1d802f548f6c7425e4defe
DimaKush <kushnarevdn@gmail.com>
2025-03-12 12:04:59 +0700

    release checklist

--- client/src/components/buttons/ExecuteButton/ExecuteButton.tsx
+++ client/src/components/buttons/ExecuteButton/ExecuteButton.tsx
@@ -1,9 +1,10 @@
-import { Button, Tooltip } from '@chakra-ui/react'
+import { Button, Tooltip, Checkbox, VStack, Text } from '@chakra-ui/react'
 import { willerContract } from 'components/Interfaces'
 import { ethers } from 'ethers'
 import { useRouter } from 'next/router'
 import type { FC } from 'react'
 import { useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { useState } from 'react'
 
 interface Balances {
     tokenBalances?: Erc20Value[];
@@ -52,6 +53,34 @@ type Erc20Value =
     }
 
 const ExecuteButton: FC<Balances> = ({tokenBalances, nftBalances, testatorAddress}) => {
+    const [selectedTokens, setSelectedTokens] = useState<{
+        erc20: string[],
+        erc721: TNFTBalance[],
+        erc1155: TNFTBalance[]
+    }>({
+        erc20: [],
+        erc721: [],
+        erc1155: []
+    })
+
+    const handleERC20Select = (address: string) => {
+        setSelectedTokens(prev => ({
+            ...prev,
+            erc20: prev.erc20.includes(address) 
+                ? prev.erc20.filter(a => a !== address)
+                : [...prev.erc20, address]
+        }))
+    }
+
+    const handleNFTSelect = (nft: TNFTBalance, type: 'erc721' | 'erc1155') => {
+        setSelectedTokens(prev => ({
+            ...prev,
+            [type]: prev[type].includes(nft)
+                ? prev[type].filter(n => n !== nft)
+                : [...prev[type], nft]
+        }))
+    }
+
     const getListsForBatchRelease = (approvedTokenList: TNFTBalance[]) => {
         const tokenList = new Array()
         const tokenIdLists = new Array()
@@ -68,14 +97,13 @@ const ExecuteButton: FC<Balances> = ({tokenBalances, nftBalances, testatorAddres
         }
         return { tokenList, tokenIdLists }
     }
-    const approvedERC20Addresses = tokenBalances ? tokenBalances.map((i: Erc20Value) => {
-        if (i.isApprovedForAll) {return i.token_address} return undefined
-    }).filter((n: any) => n) : []
-    const approvedERC721 = nftBalances && nftBalances.filter((i: any) => (i.isApprovedForAll && i.contractType === 'ERC721'))
-    const approvedERC1155 = nftBalances && nftBalances.filter((i: any) => (i.isApprovedForAll && i.contractType === 'ERC1155'))
 
-    const { tokenList: ERC721List, tokenIdLists: ERC721tokenIdLists } = getListsForBatchRelease(approvedERC721!)
-    const { tokenList: ERC1155List, tokenIdLists: ERC1155tokenIdLists } = getListsForBatchRelease(approvedERC1155!)
+    const approvedERC20Addresses = selectedTokens.erc20
+    const approvedERC721 = selectedTokens.erc721
+    const approvedERC1155 = selectedTokens.erc1155
+
+    const { tokenList: ERC721List, tokenIdLists: ERC721tokenIdLists } = getListsForBatchRelease(approvedERC721)
+    const { tokenList: ERC1155List, tokenIdLists: ERC1155tokenIdLists } = getListsForBatchRelease(approvedERC1155)
 
     const args = [testatorAddress, approvedERC20Addresses, ERC721List, ERC1155List, ERC721tokenIdLists, ERC1155tokenIdLists]
     const { config } = usePrepareContractWrite({
@@ -91,17 +119,60 @@ const ExecuteButton: FC<Balances> = ({tokenBalances, nftBalances, testatorAddres
     const { write } = useContractWrite(config)
     const nothingToRelease = approvedERC20Addresses.length === 0 && ERC721List.length === 0 && ERC1155List.length === 0
     return (
-        <Tooltip label={`Nothing to release`} placement='bottom' openDelay={500}
-         isDisabled={!nothingToRelease}
-          >
-        <Button
-            onClick={() => {
-                write?.()
-            }}
-            disabled={!ethers.utils.isAddress(testatorAddress) || nothingToRelease}>
-            Execute Will
-        </Button>
-        </Tooltip>
+        <VStack spacing={4} align="stretch">
+            {/* ERC20 Tokens */}
+            {tokenBalances?.map((token: Erc20Value) => (
+                token.isApprovedForAll && (
+                    <Checkbox 
+                        key={token.token_address}
+                        isChecked={selectedTokens.erc20.includes(token.token_address)}
+                        onChange={() => handleERC20Select(token.token_address)}
+                    >
+                        {token.symbol} ({token.balance})
+                    </Checkbox>
+                )
+            ))}
+
+            {/* ERC721 Tokens */}
+            {nftBalances?.map((nft: TNFTBalance) => (
+                nft.isApprovedForAll && nft.contractType === 'ERC721' && (
+                    <Checkbox 
+                        key={`${nft.tokenAddress}-${nft.tokenId}`}
+                        isChecked={selectedTokens.erc721.includes(nft)}
+                        onChange={() => handleNFTSelect(nft, 'erc721')}
+                    >
+                        {nft.name} #{nft.tokenId}
+                    </Checkbox>
+                )
+            ))}
+
+            {/* ERC1155 Tokens */}
+            {nftBalances?.map((nft: TNFTBalance) => (
+                nft.isApprovedForAll && nft.contractType === 'ERC1155' && (
+                    <Checkbox 
+                        key={`${nft.tokenAddress}-${nft.tokenId}`}
+                        isChecked={selectedTokens.erc1155.includes(nft)}
+                        onChange={() => handleNFTSelect(nft, 'erc1155')}
+                    >
+                        {nft.name} #{nft.tokenId} (x{nft.amount})
+                    </Checkbox>
+                )
+            ))}
+
+            <Tooltip 
+                label={`Nothing selected to release`} 
+                placement='bottom' 
+                openDelay={500}
+                isDisabled={!nothingToRelease}
+            >
+                <Button
+                    onClick={() => write?.()}
+                    disabled={!ethers.utils.isAddress(testatorAddress) || nothingToRelease}
+                >
+                    Execute Selected
+                </Button>
+            </Tooltip>
+        </VStack>
     )
 }
 
